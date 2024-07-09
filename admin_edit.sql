75434d6019cdfe706331e30ca2404caefeccc722
minseokKim6823 <alstjr6823@naver.com>
2024-07-09 10:00:29 +0900

    Feat: Edit

--- admin_edit.sql
+++
@@ -1,9 +0,0 @@
-UPDATE admin
-SET
-  company_number = ?,    
-  admin_password = ?,    
-  admin_name = ?,        
-  admin_service_years = ?, 
-  admin_created_at = ?,  
-  admin_status = ?       
-WHERE admin_id = ?;    
--- admin_withdraw.sql
+++
@@ -1,3 +0,0 @@
-UPDATE admin
-SET admin_status = '비활성', admin_created_atd = CURDATE()
-WHERE admin_id = ?;
