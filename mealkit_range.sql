ecc0a7edad6f769740452434e8d03a3f0cf7acd2
YuJeeun <dbwpdms1122@naver.com>
2024-07-09 10:24:12 +0900

    Revert "Dev je"

--- mealkit_range.sql
+++
@@ -1,5 +0,0 @@
--- 일반 밀키트 상품 범위 조회
-SELECT
-		 *
-  FROM mealkit
- WHERE mealkit_calories BETWEEN 200 AND 300;
--- mealkit_review_inquiry.sql
+++
@@ -1,6 +0,0 @@
--- 일반 밀키트 상품 리뷰 조회
-SELECT
-       *
-  FROM user_review
- WHERE review_id=30;
- 
